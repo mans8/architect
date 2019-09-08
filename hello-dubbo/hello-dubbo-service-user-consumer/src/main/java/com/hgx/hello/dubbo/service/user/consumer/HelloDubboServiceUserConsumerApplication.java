42aa808047c1928abdae0a3b37a8a57276a2cb7d
小新哥 <403713133@qq.com>
2019-09-08 18:01:29 +0800

    .

--- hello-dubbo/hello-dubbo-service-user-consumer/src/main/java/com/hgx/hello/dubbo/service/user/consumer/HelloDubboServiceUserConsumerApplication.java
+++ hello-dubbo/hello-dubbo-service-user-consumer/src/main/java/com/hgx/hello/dubbo/service/user/consumer/HelloDubboServiceUserConsumerApplication.java
@@ -2,8 +2,12 @@ package com.hgx.hello.dubbo.service.user.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableHystrix
+@EnableHystrixDashboard
 public class HelloDubboServiceUserConsumerApplication {
 
     public static void main(String[] args) {
--- hello-dubbo/hello-dubbo-service-user-consumer/src/main/java/com/hgx/hello/dubbo/service/user/consumer/controller/UserController.java
+++ hello-dubbo/hello-dubbo-service-user-consumer/src/main/java/com/hgx/hello/dubbo/service/user/consumer/controller/UserController.java
@@ -3,6 +3,7 @@ package com.hgx.hello.dubbo.service.user.consumer.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.hgx.hello.dubbo.service.user.api.UserService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +19,14 @@ public class UserController {
     @Reference(version = "${user.service.version}")
     private UserService userService;
 
+    @HystrixCommand(fallbackMethod = "hiError")
     @RequestMapping(value = "hi",method = RequestMethod.GET)
     public String sayHi(){
         return userService.sayHi();
+    }
+
+    //熔断方法
+    public String hiError(){
+        return "Hello Hystrix";
     }
 }
