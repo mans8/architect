829809154d32173ea2724d11e540cb34ec31e4c7
小新哥 <403713133@qq.com>
2019-09-08 15:22:06 +0800

    .

--- hello-dubbo/hello-dubbo-service-user-provider/src/main/java/com/hgx/hello/dubbo/service/user/provider/HelloDubboServiceUserProviderApplication.java
+++ hello-dubbo/hello-dubbo-service-user-provider/src/main/java/com/hgx/hello/dubbo/service/user/provider/HelloDubboServiceUserProviderApplication.java
@@ -4,8 +4,10 @@ import com.alibaba.dubbo.container.Main;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
+@EnableHystrix
 public class HelloDubboServiceUserProviderApplication {
 
     public static void main(String[] args) {
--- hello-dubbo/hello-dubbo-service-user-provider/src/main/java/com/hgx/hello/dubbo/service/user/provider/api/impl/UserServiceImpl.java
+++ hello-dubbo/hello-dubbo-service-user-provider/src/main/java/com/hgx/hello/dubbo/service/user/provider/api/impl/UserServiceImpl.java
@@ -2,6 +2,8 @@ package com.hgx.hello.dubbo.service.user.provider.api.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.hgx.hello.dubbo.service.user.api.UserService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Value;
 
 /**
@@ -15,8 +17,13 @@ public class UserServiceImpl implements UserService {
     @Value("${dubbo.protocol.port}")
     private String port;
 
+    @HystrixCommand(commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value="10"),
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "2000")
+    })
     @Override
     public String sayHi() {
         return "hello dubbo, I'm from port:" + port;
     }
 }
+
